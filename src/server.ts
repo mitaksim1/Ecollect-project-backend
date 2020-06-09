a338f464e5fe36d5f2ea9b288f2bfb7d3c127d3f
mitaksim1 <miriamt.simonnet@gmail.com>
2020-06-09 14:33:37 +0200

    Création des méthodes pour lister tous les utilisateurs, lister un seul utilisateur selon son id et créer un nouveau utilisateur

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,42 @@ import express from 'express';
 
 const app = express();
 
+const users = [
+    'Miriam',
+    'Martin',
+    'Kenzo'
+];
+
+/**
+ * Liste tous les utilisateurs
+ */
 app.get('/users', (request, response) => {
     console.log('Liste des utilisateurs');
 
-    response.json([
-        'Miriam',
-        'Martin',
-        'Kenzo',
-    ]);
+    return response.json(users);
+});
+
+/**
+ * Liste un utilisateur selon son id
+ */
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+/**
+ * Création d'un utilisateur
+ */
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Miriam',
+        email: 'miriam@miriam.com.br'
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
