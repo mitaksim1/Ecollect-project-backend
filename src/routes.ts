3919116075b85fc79f60130e213c400205ef0f43
mitaksim1 <miriamt.simonnet@gmail.com>
2020-06-11 18:59:40 +0200

    Création des routes pour lister des items et pour la création d'un point de collecte

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,56 @@
 import express from 'express';
+// Pour pouvoir se connecter à notre bdd, on doit importer le fichier connection
+import knex from './database/connection';
 
 const routes = express.Router();
 
 /**
  * Liste tous les utilisateurs
  */
-routes.get('/users', (request, response) => {
+routes.get('/items', async (request, response) => {
+    // requête à la bdd
+    const items = await knex('items').select('*');
 
-    return response.json({ message: 'Hello World'});
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+        
+    });
+
+    return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+    const { name, email, telephone, latitude, longitude, city, region, items } = request.body;
+
+    const insertedPointsIds = await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email, 
+        telephone,
+        latitude, 
+        longitude,
+        city,
+        region
+    });
+
+    const point_id = insertedPointsIds[0];
+
+    // Pour associer un point avec un/plusieus item(s)
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id
+        };
+    })
+
+    await knex('point_items').insert(pointItems);
+
+
+    return response.json({ success: true });
 });
 
 export default routes;
